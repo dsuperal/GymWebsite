9f77bc9d407d21a0965a08dcf7be14f4a1d171b2
Your Name <dsuperal@trinity.edu>
2024-11-15 16:33:42 -0600

    Pages

--- database.js
+++ database.js
@@ -1,27 +1,31 @@
-const { Pool,Client } = require('pg');
+const { Client } = require('pg');
 
-const connectionString = 'postgresql://postgres:Esteban10@localhost:5432/test';
+const connectionString = 'postgresql://postgres:Strawberrymysql.0824@localhost:5432/postgres';
 
 const client = new Client({
     connectionString: connectionString,
 });
 
-//client.connect();  // Connect only once
+client.connect()  // Connect once
 
-/*
-// Query 1
-client.query('SELECT * FROM Form', (err, res) => {
-    console.log(err, res);
-    
-    // Additional Query
-    const qr = "INSERT INTO Form VALUES('DaisyN', '123@gmail.com', 87246)";
+// Query 1: SELECT
+client.query('SELECT * FROM public."Registration Form"', (err, res) => {
+    if (err) {
+        console.error('Error in SELECT query', err.stack);
+    } else {
+        console.log('SELECT query result:', res.rows);
+    }
+
+    // Query 2: INSERT
+    const qr = "INSERT INTO public.\"Registration Form\" (\"Name\", \"Mail Id\", \"Phone Number\") VALUES ('Daisy', 123, 87246)";
     client.query(qr, (err, res) => {
-        console.log(err, res);
-        
+        if (err) {
+            console.error('Error in INSERT query', err.stack);
+        } else {
+            console.log('INSERT query result:', res.rows);
+        }
+
         // End the connection after all queries are done
         client.end();
- //   });
-});  */
-
-
-//can only use client once
+    });
+});
--- server.js
+++ server.js
@@ -1,45 +1,52 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { Pool } = require("pg");
+
 const app = express();
 const port = 9000;
 
-const bodyParser=require('body-parser');
+// PostgreSQL Pool Setup
+const pool = new Pool({
+    connectionString: "postgresql://postgres:Strawberrymysql.0824@localhost:5432/postgres",
+});
 
-
-
+// Middleware
 app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/",(req,res) => {
+// Serve the Home Page
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/pages/home.html");
+});
+
+// Serve the Login Page
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/pages/login.html");
+});
+
+// Serve the Registration Form
+app.get("/signup", (req, res) => {
     res.sendFile(__dirname + "/pages/form.html");
 });
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.get('/submit',function(req,res){
-    console.log('Data Saved')
+// Handle Form Submission for Registration
+app.post("/", async (req, res) => {
+    const { f_name, mail, phone } = req.body;
 
-})
+    try {
+        const query = `INSERT INTO public."Registration Form" ("Name", "Mail Id", "Phone Number") VALUES ($1, $2, $3) RETURNING *`;
+        const result = await pool.query(query, [f_name, mail, phone]);
+        console.log("Data Inserted:", result.rows);
 
-const { Pool,Client } = require('pg');
-
-const connectionString = 'postgresql://postgres:Esteban10@localhost:5432/test';
-
-const client = new Client({
-    connectionString: connectionString,
+        // Respond to client
+        res.sendFile(__dirname + "/pages/form.html");
+    } catch (err) {
+        console.error("Error inserting data:", err.stack);
+        res.status(500).send("Error saving data.");
+    }
 });
 
-app.post("/",(req,res)=>{
-    const{f_name, mail, phone}= req.body
-    client.connect()
-    client.query('INSERT INTO Form values($1,$2,$3)',[f_name, mail, phone], (err,res) =>{
-        console.log(err,res);
-        client.end()
-        //alert("Data Saved");
-    });
-    
-    res.sendFile(__dirname + "/pages/form.html");
-});
-
-
-
-app.listen(port, () =>{
-    console.log(`Example app listening on port ${port}!`);
+// Start the Server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
