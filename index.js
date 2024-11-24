3dfbd0a19dd2d792944e1910871fc30ff22ca046
Daisy Nieto <dnieto@trinity.edu>
2024-11-23 22:52:39 -0600

    changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const {memberLogin} = require('./databaseFiles/login');
+var {memberLogin} = require('./databaseFiles/login');
+var {handleSignup} = require('./databaseFiles/signup'); 
+
 var connection = require('./databaseFiles/database').databaseConnection;
 var fs = require("fs");
 
@@ -19,4 +21,6 @@ router.post('/login', memberLogin);
 router.get('/signup', (req,res) =>{
     res.render('signup',{ error: null });
 })
+router.post('/signup', handleSignup);
+
 module.exports = router;
